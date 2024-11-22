18e8b87e81dd514ec402126625fc7f414cfd8e7a
yujamint <woawoa0513@gmail.com>
2024-11-22 22:08:48 +0900

    refactor: Request, Response 형식 변경

--- src/main/java/com/example/pawpaw/domain/survey/dto/request/SurveyCategoryResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.pawpaw.domain.survey.dto.request;
-
-import java.util.List;
-
-public record SurveyCategoryResponse(
-    String category,
-    List<Integer> surveyResponses
-) {
-}
